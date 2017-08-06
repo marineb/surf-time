86106775eb1b2e5438d9e0942c34666f5cd95233
Brian Whitton <me@brianwhitton.com>
2017-08-06 11:58:25 -0400

    adds tide icons

---
+++ src/lib/low-tide-icon.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class extends Component {
+  render() {
+    return (<svg xmlns="http://www.w3.org/2000/svg" width="23" height="15" viewBox="0 0 23 15">
+      <g fill="#FFF" fillRule="evenodd">
+        <path d="M23 14.999a12.41 12.41 0 0 1-.192.001C16.862 15 11.88 10.733 10.57 5h2.48c1.607 4.563 5.377 7.767 9.772 7.767.06 0 .119 0 .178-.002v2.234z"/>
+        <path d="M0 14.999L.192 15C6.138 15 11.12 10.733 12.43 5H9.95C8.343 9.563 4.573 12.767.178 12.767c-.06 0-.119 0-.178-.002v2.234z"/>
+        <g>
+          <path d="M0 0h4.423v1H0zM6.192 0h4.423v1H6.192zM12.385 0h4.423v1h-4.423zM18.577 0H23v1h-4.423z"/>
+        </g>
+      </g>
+    </svg>)
+  }
+}
