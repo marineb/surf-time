82f7e15916e094a08168ca2a15af24375ae14598
Brian Whitton <me@brianwhitton.com>
2017-08-06 11:59:19 -0400

    implement tide icon

--- src/lib/tide-item.js
+++ src/lib/tide-item.js
@@ -2,11 +2,34 @@ import React, { Component } from 'react';
 import Moment from 'react-moment';
 import 'moment-timezone';
 
+import './tide-item.css';
+import LowTideIcon from './low-tide-icon';
+import HighTideIcon from './high-tide-icon';
+
 export default class extends Component {
   render() {
+    let icon;
+    switch(this.props.tide) {
+      case 'High':
+        icon = <HighTideIcon />;
+        break;
+      case 'Low':
+        icon = <LowTideIcon />;
+        break;
+      default:
+        icon = '';
+        break;
+    }
+    let unit = this.props.unit || 'm';
+    let height;
+    if (unit === 'ft') {
+      height = this.props.height * 3.37;
+    } else {
+      height = this.props.height
+    }
     return (
       <div className="TideItem">
-        <Moment date={this.props.time} format="HH:mm"/> | <em>{this.props.height} ft</em>
+        <Moment date={this.props.time} format="HH:mm"/> {icon} <em>{height.toFixed()} {unit}</em>
       </div>
     )
   }
