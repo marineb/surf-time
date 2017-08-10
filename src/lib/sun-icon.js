8dd968aafde037f5bda94f5a66abccd83910aefb
Brian Whitton <me@brianwhitton.com>
2017-08-10 09:55:47 -0400

    add a tense to sunrise/sunset

--- src/lib/sun-icon.js
+++ src/lib/sun-icon.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Moment from 'react-moment';
 import moment from 'moment';
+import './sun-icon.css';
 
 export default class extends Component {
   render() {
     let inFuture = moment().isBefore(this.props.time);
     return (
-      <span>{this.props.type} {inFuture ? "will be" : "was"} <Moment date={this.props.time} format="HH:mm" /></span>
+      <div className="SunIcon">{this.props.type} {inFuture ? "will be" : "was"} <Moment date={this.props.time} format="HH:mm" /></div>
     )
   }
 }
