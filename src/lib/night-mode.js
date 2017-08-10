373970ad895889969a7ad7f4d3cf54442c28f97f
Brian Whitton <me@brianwhitton.com>
2017-08-10 12:08:28 -0400

    night mode wording

--- src/lib/night-mode.js
+++ src/lib/night-mode.js
@@ -6,11 +6,11 @@ export default class extends Component {
   render() {
     return (
       <div className="NightMode">
-        You really shouldn't be surfing at night, Marine. <br />
+        You really shouldn't be surfing at night, Marine.<br />
         <br />
         Sunset was at <Moment date={this.props.sunset} format="HH:mm" />. <br />
         <br />
-        Sunrise will be at <Moment date={this.props.sunrise} format="HH:mm" />.
+        Try again after sunrise, which will be at <Moment date={this.props.sunrise} format="HH:mm" />.
       </div>
     )
   }
